1d641d73bd26e8b46efe601da0be9b04a1686995
Chad <chad.wilson@softrams.com>
2022-12-02 15:57:51 -0500

    Initial commit

---
+++ ecs-app/src/main/java/com/softrams/ecs/EcsAppApplication.java
@@ -0,0 +1,13 @@
+package com.softrams.ecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsAppApplication.class, args);
+	}
+
+}
---
+++ ecs-app/src/test/java/com/softrams/ecs/EcsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softrams.ecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
